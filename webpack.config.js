22eb684f9defb4151edad2db7f685b9806b81348
Caleb Forbes <caleb.s.forbes@gmail.com>
2019-10-28 16:44:10 -0500

    Webpack with Babel
    following https://www.robinwieruch.de/webpack-babel-setup-tutorial

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,18 @@ module.exports = {
     // 1
     entry: './src/index.js',
     // 2
+    module: {
+        rules: [
+          {
+            test: /\.(js)$/,
+            exclude: /node_modules/,
+            use: ['babel-loader']
+          }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js']
+    },
     output: {
       path: __dirname + '/dist',
       publicPath: '/',
